376fc263b3baaafbe901e2f8956059b06634df92
贾丽鹏 <1094425279@qq.com>
2026-04-11 16:52:37 +0800

    commit

--- example10-simple-http-server/gin/main.go
+++ example10-simple-http-server/gin/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
-func ginEngine() *gin.Engine{
+func ginEngine() *gin.Engine {
 	router := gin.Default()
-	
-	router.GET("")
+
+	router.GET("/welcome", func(ctx *gin.Context) {
+		firstname := ctx.DefaultQuery("firstname", "Guest")
+		lastname := ctx.Query("lastname")
+		ctx.String(http.StatusOK, "Hello %s %s", firstname, lastname)
+	})
+
+	router.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"message": gin.H{"data1": "data2"}})
+	})
+
+	return router
+}
+
+func main() {
+	r := ginEngine()
+	r.Run(":8080")
 }
